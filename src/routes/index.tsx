b41f1bac07a681dc08c412748a236d2dbb1ca389
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-03-28 20:51:57 -0300

    Feat: configurando rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../pages/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#1e3ba1",
+          },
+
+          headerTitleAlign: "center",
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
